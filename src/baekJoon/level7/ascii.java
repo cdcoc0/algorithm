8d5d9f6c00817eb66d0d36da577bacf0f61d9cd5
velykitty <jiyunee13@gmail.com>
2021-07-11 21:24:13 +0900

    baekjoon

---
+++ src/baekJoon/level7/ascii.java
@@ -0,0 +1,13 @@
+package baekJoon.level7;
+
+import java.util.Scanner;
+
+public class ascii {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int tmp = stdIn.next().charAt(0);
+
+        System.out.println(tmp);
+    }
+}

2e88e2ec0df81e1a32991c5f9e1c2e3b6b2a34ed
cdcoc0 <jiyunee13@gmail.com>
2021-11-05 14:20:48 +0900

    백준
    프로그래머스
    실패율

---
+++ src/programmers/level1/Failure.java
@@ -0,0 +1,45 @@
+package programmers.level1;
+
+public class Failure {
+    static int[] solution(int N, int[] stages) {
+        float[] n = new float[N + 2];
+        int[] result = new int[N];
+
+        for(int stage : stages) {
+            n[stage]++;
+        }
+
+
+        int sub = stages.length;
+        for(int i = 1; i < n.length - 1; i++) {
+            float temp = n[i];
+            n[i] = temp / sub;
+            sub -= temp;
+            if(sub == 0)
+                break;
+        }
+        for(int i = 0; i < n.length; i++) {
+            System.out.println(n[i]);
+        }
+
+        for(int i = 0; i < n.length - 2; i++) {
+            int max = 1;
+            for(int j = 2; j < n.length - 1; j++) {
+                if(n[j] > n[max]) {
+                    max = j;
+                }
+            }
+            result[i] = max;
+            System.out.println(max);
+            n[max] = -1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+//        int[] s = {2, 1, 2, 6, 2, 4, 3, 3};
+        int[] s = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+        System.out.println(solution(6, s));
+    }
+}

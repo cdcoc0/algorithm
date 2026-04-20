5b959d6352fad40371ce2cf19bb47b37dc7dce2a
jiyeonchoi <jiyunee13@gmail.com>
2026-04-20 11:55:28 +0900

    프로그래머스 lvl2.
    1. 2개 이하로 다른 비트

---
+++ src/programmers/level2/DifferentBits.java
@@ -0,0 +1,42 @@
+package programmers.level2;
+
+/**
+ * 2개 이하로 다른 비트 (월간 코드 챌린지 시즌2)
+ */
+public class DifferentBits {
+    public static void main(String[] args) {
+        long[] numbers = {2, 7};
+        long[] result = Solution.solution(numbers);
+        for(long a : result) System.out.println(a);
+    }
+
+    static class Solution {
+        static public long[] solution(long[] numbers) {
+            long[] answer = new long[numbers.length];
+
+            for(int i = 0; i < numbers.length; i++) {
+                long tmp = numbers[i];
+                String binary = Long.toBinaryString(tmp);
+                String binary2 = binary;
+
+                if(tmp % 2 == 0) {
+                    answer[i] = tmp+1;
+                    continue;
+                }
+
+                for(int j = binary.length()-1; j >= 0; j--) {
+                    if(binary.charAt(j) == '0') {
+                        binary2 = binary.substring(0, j) + "10" + binary.substring(j+2);
+                        break;
+                    }
+                }
+
+                if(binary.equals(binary2)) binary2 = "10" + binary.substring(1);
+
+                answer[i] = Long.parseLong(binary2, 2);
+            }
+
+            return answer;
+        }
+    }
+}
